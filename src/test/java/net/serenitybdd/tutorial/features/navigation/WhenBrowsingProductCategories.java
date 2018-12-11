da5ceec84b3c25cbf2bd2a3b9c896971cb00f672
Liudmyla Samko <liuda.samko@gmail.com>
2018-12-11 13:19:35 +0200

    tutorial_2, work with ScreenPlay

--- src/test/java/net/serenitybdd/tutorial/features/navigation/WhenBrowsingProductCategories.java
+++ src/test/java/net/serenitybdd/tutorial/features/navigation/WhenBrowsingProductCategories.java
@@ -1,13 +1,23 @@
 package net.serenitybdd.tutorial.features.navigation;
 
 import net.serenitybdd.junit.runners.SerenityRunner;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.questions.page.TheWebPage;
 import net.serenitybdd.tutorial.model.Category;
 import net.serenitybdd.tutorial.steps.NavigatingUser;
+import net.serenitybdd.tutorial.tasks.NavigateTo;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
+
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static net.serenitybdd.screenplay.GivenWhenThen.then;
+import static net.serenitybdd.screenplay.GivenWhenThen.when;
+import static net.serenitybdd.tutorial.model.Category.Motors;
+import static org.hamcrest.Matchers.containsString;
 
 @RunWith(SerenityRunner.class)
 public class WhenBrowsingProductCategories {
@@ -24,10 +34,18 @@ public class WhenBrowsingProductCategories {
         mark.isOnTheHomePage();
 
         //When
-        mark.navigatesToCategory(Category.Motors);
+        mark.navigatesToCategory(Motors);
 
         //Then
         mark.shouldSeePageTitleContaining("New and used cars");
+    }
 
+    @Test
+    public void shouldBeAbleToViwMotorProducts(){
+        Actor mike = Actor.named("Mike");
+        mike.can(BrowseTheWeb.with(browser));
+
+        when(mike).attemptsTo(NavigateTo.theCategory(Motors));
+        then(mike).should(seeThat(TheWebPage.title(), containsString("New and used cars")));
     }
 }
